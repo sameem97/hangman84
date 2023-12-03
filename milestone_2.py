6e6a8e19b6853fc82c80714fb371f88267a3664f
Sameem <sameem_97@outlook.com>
2023-12-03 18:57:44 +0000

    create function for get user guess

--- milestone_2.py
+++ milestone_2.py
@@ -4,12 +4,12 @@ word_list = ["Pineapple", "Mango", "Blueberries", "Kiwi", "Orange"]
 
 word = random.choice(word_list)
 
-
-while True:
-    guess = input("Please enter a single letter")
-    if len(guess) == 1 and guess.isalpha():
-        print("Good guess!")
-        break
-    else:
-        print("Oops! That is not a valid input. Please try again!")
-        continue
+def get_user_guess() -> str:
+    while True:
+        guess = input("Please enter a single letter")
+        if len(guess) == 1 and guess.isalpha():
+            print("Good guess!")
+            return guess
+        else:
+            print("Oops! That is not a valid input. Please try again!")
+            continue
