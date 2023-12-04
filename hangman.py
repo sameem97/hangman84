e9ba11440e40262a60cac75411710505a05ddd03
Sameem <sameem_97@outlook.com>
2023-12-04 00:05:22 +0000

    Create class implementation.

--- hangman.py
+++ hangman.py
@@ -1,21 +1,46 @@
 import random
 from typing import List
-from utils.guess import get_user_guess
 from utils.variables import word_list
 
-def main() -> None:
-    """Play hangman using a random word from an inputted list of words"""
-    word = random.choice(word_list)
-    guess = get_user_guess()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
-    else:
-        print(f"Sorry. {guess} is not in the word. Try again.")
+class Hangman:
+    def __init__(self, word_list: List[str]) -> None:
+        self.word_list = word_list
+        self.word: str = random.choice(word_list)
+        self.word_guessed: List[str] = ["_" for character in self.word]
+        self.num_letters: int = len(set(self.word))
+        self.num_lives: int = 5
+        self.list_of_guesses: List[str] = []
 
+    def __check_guess(self, guess: str):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for letter in self.word:
+                if letter == guess:
+                    index = self.word.index(letter)
+                    self.word_guessed[index] = guess
+            print(self.word_guessed)
+            self.num_letters -= 1
+        else:
+            print(f"Sorry. {guess} is not in the word. Try again.")
+            self.num_lives -= 1
+            print(f"You have {self.num_lives} lives left.")
+    
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a letter")
+            if not len(guess) == 1 and not guess.isalpha():
+                print("Invalid letter. Please enter a single alphabetical character.")
+                continue
+            elif guess in self.list_of_guesses:
+                print(f"You already tried that letter!")
+            else:
+                self.__check_guess(guess)
+                self.list_of_guesses.append(guess)
 
 if __name__ == "__main__":
-    main()
-
+    game = Hangman(word_list)
+    game.ask_for_input()
 
 
 
--- utils/guess.py
+++
@@ -1,8 +0,0 @@
-def get_user_guess() -> str:
-    while True:
-        guess = input("Please guess a letter")
-        if len(guess) == 1 and guess.isalpha():
-            return guess
-        else:
-            print("Invalid letter. Please enter a single alphabetical character.")
-            continue
