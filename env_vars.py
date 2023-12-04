46eaccecfe396a1fafe8932e1c5a773cc85fc2ad
Sameem <sameem_97@outlook.com>
2023-12-04 01:03:54 +0000

    Fix issue for >1 match. Fill .gitignore. Refactor.

---
+++ env_vars.py
@@ -0,0 +1,4 @@
+"""This module contains environment variables required for the game."""
+
+# This variable should be editted to suit the game.
+word_list = ["Pineapple", "Mango", "Blueberries", "Kiwi", "Orange"]
--- hangman.py
+++ hangman.py
@@ -1,13 +1,14 @@
 """Module for hangman game."""
 import random
+import sys
 from typing import List
-from utils.variables import word_list
+from env_vars import word_list
 
 class Hangman:
     """Class to represent Hangman object."""
     def __init__(self, word_list: List[str]) -> None:
         self.word_list = word_list
-        self.word: str = random.choice(word_list)
+        self.word: str = random.choice(word_list).lower()
         self.word_guessed: List[str] = ["_" for character in self.word]
         self.num_letters: int = len(set(self.word))
         self.num_lives: int = 5
@@ -15,31 +16,39 @@ class Hangman:
 
     def __update_word_guessed(self, guess: str):
         """for a correct guess, update the word with the letter guessed"""
+        current_index = 0
         for letter in self.word:
-                if letter == guess:
-                    index = self.word.index(letter)
-                    self.word_guessed[index] = guess
+            if letter == guess:
+                current_index = self.word.index(letter, current_index)
+                self.word_guessed[current_index] = guess
+                current_index += 1
 
     def __check_guess(self, guess: str):
         """Check if the guess is correct. 
         If so, update the word and reduce num_letters.
         Otherwise reduce num_lives"""
-        guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
             self.__update_word_guessed(guess)
             print(self.word_guessed)
             self.num_letters -= 1
+            if self.num_letters == 0:
+                print("You have won! Congrats!")
+                sys.exit(-1)
         else:
             print(f"Sorry. {guess} is not in the word. Try again.")
             self.num_lives -= 1
-            print(f"You have {self.num_lives} lives left.")
+            if self.num_lives == 0:
+                print("You have ran out of lives. Better luck next time!")
+                sys.exit(-1)
+            else:
+                print(f"You have {self.num_lives} lives left.")
     
     def ask_for_input(self):
         """get user input and check if it is single character alphabet. 
           If so, then proceed with check_guess and append guess to list_of_guesses."""
         while True:
-            guess = input("Please guess a letter")
+            guess = input("Please guess a letter").lower()
             if not len(guess) == 1 and not guess.isalpha():
                 print("Invalid letter. Please enter a single alphabetical character.")
                 continue
