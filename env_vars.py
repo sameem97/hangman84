a9461e3f8e459dce0b7d3806c2b5110d0cd40485
Praddyumn Yadav <praddyumny@gmail.com>
2023-12-06 23:50:54 +0530

    Add More Words to the Game.

--- env_vars.py
+++ env_vars.py
@@ -1,4 +1,17 @@
 """This module contains environment variables required for the game."""
 
 # This variable should be edited to suit the game.
-word_list = ["Pineapple", "Mango", "Blueberries", "Kiwi", "Orange"]
+word_list = [
+    'apple', 'banana', 'orange', 'grape', 'strawberry', 'melon', 'pear', 'peach', 'plum', 'cherry',
+    'carrot', 'broccoli', 'cucumber', 'lettuce', 'tomato', 'potato', 'onion', 'garlic', 'pepper', 'spinach',
+    'dog', 'cat', 'bird', 'fish', 'rabbit', 'turtle', 'hamster', 'snake', 'mouse', 'horse',
+    'happy', 'sad', 'angry', 'excited', 'calm', 'nervous', 'surprised', 'bored', 'confused', 'content',
+    'sun', 'moon', 'star', 'cloud', 'rain', 'snow', 'wind', 'storm', 'thunder', 'lightning',
+    'book', 'pen', 'pencil', 'notebook', 'computer', 'desk', 'chair', 'lamp', 'clock', 'calendar',
+    'red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white',
+    'run', 'walk', 'jump', 'swim', 'dance', 'sing', 'play', 'read', 'write', 'draw',
+    'house', 'car', 'bike', 'bus', 'train', 'plane', 'ship', 'truck', 'motorcycle', 'boat',
+    'friend', 'family', 'teacher', 'student', 'doctor', 'police', 'firefighter', 'engineer', 'artist', 'musician',
+    'eat', 'drink', 'sleep', 'exercise', 'study', 'work', 'play', 'relax', 'travel', 'explore',
+    'love', 'hate', 'happy', 'sad', 'angry', 'excited', 'bored', 'surprised', 'calm', 'nervous', 'proud'
+]
